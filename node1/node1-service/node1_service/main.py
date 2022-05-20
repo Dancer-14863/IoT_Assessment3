934ad0ccbcf112a1b0d510cddf739431a0da554e
Malika Liyanage <malika.liyanage@atlaslabs.com.au>
2022-05-21 04:42:18 +0530

    feat(mqtt-integration): integrated the MQTT service with the main service

--- node1/node1-service/node1_service/main.py
+++ node1/node1-service/node1_service/main.py
@@ -1,8 +1,12 @@
 from node1_service.services.main_service import MainService
 from node1_service.services.database_service import DatabaseService
+from node1_service.services.mqtt_service import MQTTService
+from dotenv import load_dotenv
 
 
 def start():
+    load_dotenv()
     database_service = DatabaseService()
-    main_service = MainService(database_service)
+    mqtt_service = MQTTService()
+    main_service = MainService(database_service, mqtt_service)
     main_service.start()
--- node1/node1-service/node1_service/services/main_service.py
+++ node1/node1-service/node1_service/services/main_service.py
@@ -1,4 +1,7 @@
+from datetime import datetime
+import json
 from node1_service.services.database_service import DatabaseService
+from node1_service.services.mqtt_service import MQTTService
 from node1_service.utils.arduino_helpers import find_arduino, send_command_arduino, make_transaction
 from node1_service.utils.commands import Commands
 import serial
@@ -9,9 +12,16 @@ import asyncio
 class MainService:
     arduino = None
     database_service = None
+    mqtt_service = None
+    configuration = None
 
-    def __init__(self, database_service: DatabaseService):
+    #  Hardcoded device information
+    device_id = '0001'
+    service_start_date_time = datetime.now()
+
+    def __init__(self, database_service: DatabaseService, mqtt_service: MQTTService):
         self.database_service = database_service
+        self.mqtt_service = mqtt_service
 
     def start(self):
         port = find_arduino()
@@ -26,6 +36,11 @@ class MainService:
                     print('Node 1 Service is now running. Press CTRL-C to exit')
                     self.arduino = arduino
                     send_command_arduino(arduino, Commands.ACTIVATE_LED)
+
+                    self.__get_configuration()
+                    self.__init_subscriptions()
+                    self.mqtt_service.start()
+
                     loop = asyncio.get_event_loop()
                     try:
                         loop.run_until_complete(self.__main_process())
@@ -33,7 +48,9 @@ class MainService:
                         send_command_arduino(arduino, Commands.DEACTIVATE_LED)
                         loop.run_until_complete(loop.shutdown_asyncgens())
                         loop.close()
+                        self.mqtt_service.stop()
                         print('Exiting program')
+
                 else:
                     print('Could not connect to device')
         else:
@@ -41,8 +58,57 @@ class MainService:
 
     async def __main_process(self):
         while True:
-            await asyncio.sleep(5)
+            await asyncio.sleep(10)
+
             sensor_reading = int(make_transaction(
                 self.arduino, Commands.GET_SENSOR_READING))
-            self.database_service.save_sensor_reading(sensor_reading)
-            print(sensor_reading)
+            status = 'NORMAL'
+            if (sensor_reading < self.configuration.min_threshold):
+                status = 'BELOW THRESHOLD'
+            elif (sensor_reading > self.configuration.max_threshold):
+                status = 'ABOVE THRESHOLD'
+            sensor_log = self.database_service.save_sensor_reading(
+                sensor_reading, status)
+
+            if (sensor_log.status != 'NORMAL'):
+                self.__publish_sensor_log(sensor_log)
+
+            print(f'Sensor Reading: {sensor_reading}')
+
+    def __get_configuration(self):
+        self.configuration = self.database_service.get_latest_configuration()
+
+    def __init_subscriptions(self):
+        self.mqtt_service.set_base_subscription('node1')
+        self.mqtt_service.add_message_callback(
+            'get-status', self.__status_callback)
+        self.mqtt_service.add_message_callback(
+            'update-configuration', self.__configuration_callback)
+
+    def __status_callback(self, client, user_data, message):
+        self.__publish_status()
+
+    def __configuration_callback(self, client, user_data, message):
+        parsed_payload = json.loads(message.payload.decode('utf-8'))
+        self.configuration.min_threshold = parsed_payload['data']['min_threshold']
+        self.configuration.max_threshold = parsed_payload['data']['max_threshold']
+        self.database_service.commit_session()
+
+    def __publish_status(self):
+        date_time_string = self.service_start_date_time.strftime(
+            '%Y-%m-%d %H:%M:%S')
+        response_object = {
+            'data': {
+                'device_id': self.device_id,
+                'up_date_time': date_time_string,
+                'status': 'UP',
+            }
+        }
+        self.mqtt_service.publish('status', json.dumps(response_object))
+
+    def __publish_sensor_log(self, sensor_log):
+        response_object = {
+            'data': sensor_log.to_json(),
+        }
+        self.mqtt_service.publish(
+            'notifications', json.dumps(response_object))

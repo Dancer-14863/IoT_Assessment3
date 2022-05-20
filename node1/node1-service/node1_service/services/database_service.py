136f2c3b6c3479ebf7997e6a791097f4d833b786
Malika Liyanage <malika.liyanage@atlaslabs.com.au>
2022-05-21 04:41:57 +0530

    feat(database-service): added methods to fetch and update configuration records

--- node1/node1-service/node1_service/services/database_service.py
+++ node1/node1-service/node1_service/services/database_service.py
@@ -1,14 +1,24 @@
 from node1_service.models.base import Session
 from node1_service.models.sensor_log import SensorLog
+from node1_service.models.configuration import Configuration
 
 
 class DatabaseService:
-    def save_sensor_reading(self, reading):
-        session = Session()
-        sensor_log = SensorLog(reading)
-        session.add(sensor_log)
-        session.commit()
-        session.close()
+    session = Session()
+
+    def save_sensor_reading(self, reading, status):
+        sensor_log = SensorLog(reading, status)
+        self.session.add(sensor_log)
+        self.session.flush()
+        self.commit_session()
+        self.session.refresh(sensor_log)
+        return sensor_log
+    
+    def get_latest_configuration(self):
+        return self.session.query(Configuration).order_by(Configuration.id.desc()).first()
+        
+    def commit_session(self):
+        self.session.commit()
 
 
 

acb593d6f752cc5cd5f7ea95efb8d2a818040b11
Malika Liyanage <malika.liyanage@atlaslabs.com.au>
2022-05-21 04:41:30 +0530

    feat(sensor-log-model): added status column and json conversion

--- node1/node1-service/node1_service/models/sensor_log.py
+++ node1/node1-service/node1_service/models/sensor_log.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, TIMESTAMP, sql
+from email.policy import default
+from sqlalchemy import Column, Integer, String, TIMESTAMP, sql
 from node1_service.models.base import Base
 
 
@@ -7,7 +8,18 @@ class SensorLog(Base):
 
     id = Column(Integer, primary_key=True)
     sensor_reading = Column(Integer, default=0)
+    status = Column(String(24), default='NORMAL')
     recorded_at = Column(TIMESTAMP, server_default=sql.func.now(),)
 
-    def __init__(self, sensor_reading):
+    def __init__(self, sensor_reading, status):
         self.sensor_reading = sensor_reading
+        self.status = status
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'sensor_reading': self.sensor_reading,
+            'status': self.status,
+            'recorded_at': self.recorded_at.strftime(
+                '%Y-%m-%d %H:%M:%S'),
+        }

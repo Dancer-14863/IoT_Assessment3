7a49f3c0c8f56b19db109d797e6bd71d7baaa708
Malika Liyanage <malika.liyanage@atlaslabs.com.au>
2022-05-25 07:58:27 +0530

    refactor(changed-file-directory): changed file directory

---
+++ tempestas-dashboard/components/Overview/Overview.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="columns">
+    <div class="column is-3">
+      <WeatherCard />
+    </div>
+    <div class="column is-3">
+      <WaterCard />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import WeatherCard from './WeatherCard.vue'
+import WaterCard from './WaterCard.vue'
+export default Vue.extend({
+  components: { WeatherCard, WaterCard },
+  name: 'Overview',
+})
+</script>
--- tempestas-dashboard/components/Overview/WeatherCard.vue
+++ tempestas-dashboard/components/Overview/WeatherCard.vue
@@ -3,14 +3,15 @@
     <div class="card-content">
       <div class="columns is-flex is-vcentered">
         <div class="column is-3">
-          <b-icon icon="water-outline" size="is-large" type="is-primary"> </b-icon>
+          <b-icon icon="cloud-outline" size="is-large" type="is-primary">
+          </b-icon>
         </div>
         <div class="column">
-          <p class="is-size-6 has-text-primary">Water Pumped</p>
+          <p class="is-size-6 has-text-primary">Kuching</p>
           <p class="is-size-3 has-text-primary has-text-weight-bold">
-			  0 L
+            32.0 &#8451;
           </p>
-          <p class="is-size-5 has-text-primary">25/03/2022</p>
+          <p class="is-size-5 has-text-primary">Sunny</p>
         </div>
       </div>
     </div>
@@ -20,6 +21,6 @@
 <script lang="ts">
 import Vue from 'vue'
 export default Vue.extend({
-	name: 'WaterCard'
+  name: 'WeatherCard',
 })
 </script>

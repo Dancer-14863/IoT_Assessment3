c31ce4f861efa13781ba1d2d4b2f48e2769ae573
Malika Liyanage <malika.liyanage@atlaslabs.com.au>
2022-05-25 07:08:09 +0530

    feat(water-card): made the UI for the Water Card

---
+++ tempestas-dashboard/components/WaterCard.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="card has-background-primary-light p-3">
+    <div class="card-content">
+      <div class="columns is-flex is-vcentered">
+        <div class="column is-3">
+          <b-icon icon="water-outline" size="is-large" type="is-primary"> </b-icon>
+        </div>
+        <div class="column">
+          <p class="is-size-6 has-text-primary">Water Pumped</p>
+          <p class="is-size-3 has-text-primary has-text-weight-bold">
+			  0 L
+          </p>
+          <p class="is-size-5 has-text-primary">25/03/2022</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+	name: 'WaterCard'
+})
+</script>

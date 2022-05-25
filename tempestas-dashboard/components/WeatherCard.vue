303992cece1f76e93dbd684244201369068aadd9
Malika Liyanage <malika.liyanage@atlaslabs.com.au>
2022-05-25 07:07:31 +0530

    feat(weather-card): made the UI for the weather card

--- tempestas-dashboard/components/WeatherCard.vue
+++ tempestas-dashboard/components/WeatherCard.vue
@@ -1,27 +1,17 @@
 <template>
-  <div class="card">
+  <div class="card has-background-primary-light p-3">
     <div class="card-content">
-      <div class="media">
-        <div class="media-left">
-          <figure class="image is-48x48">
-            <img
-              src="https://bulma.io/images/placeholders/96x96.png"
-              alt="Placeholder image"
-            />
-          </figure>
+      <div class="columns is-flex is-vcentered">
+        <div class="column is-3">
+          <b-icon icon="cloud-outline" size="is-large" type="is-primary"> </b-icon>
         </div>
-        <div class="media-content">
-          <p class="title is-4">John Smith</p>
-          <p class="subtitle is-6">@johnsmith</p>
+        <div class="column">
+          <p class="is-size-6 has-text-primary">Kuching</p>
+          <p class="is-size-3 has-text-primary has-text-weight-bold">
+            32.0 &#8451;
+          </p>
+          <p class="is-size-5 has-text-primary">Sunny</p>
         </div>
-      </div>
-
-      <div class="content">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec
-        iaculis mauris. <a>@bulmaio</a>. <a href="#">#css</a>
-        <a href="#">#responsive</a>
-        <br />
-        <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
       </div>
     </div>
   </div>

07932b785da9ed93644bf4c1b480f4fe1f3ef2dc
Malika Liyanage <malika.liyanage@atlaslabs.com.au>
2022-05-30 15:23:25 +0530

    feat(date-time-filter): created datetime string filter

---
+++ tempestas-dashboard/utils/filters/date_time_string.ts
@@ -0,0 +1,9 @@
+import moment from 'moment'
+
+export function toDateTimeString(date: Date | string): string {
+  if (!date || date === '') return ''
+  if (typeof date === 'string') {
+    date = new Date(`${date} UTC`)
+  }
+  return moment(date).format('H:mm:ss DD-MM-YYYY')
+}
